caec49a53ce1caee3b90b5dd1cbc8a03f736b1db
seoyeonki <ksy201811677@likelion.org>
2021-05-10 10:54:11 +0900

    4.반응속도 체크 완료

--- 4-2.반응속도체크/ResponseCheck.vue
+++ 4-2.반응속도체크/ResponseCheck.vue
@@ -2,11 +2,11 @@
 <template>
     <div>
         <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div> <!--v-bind:를 :로 축약 가능 --> <!--class="state"이면 클래스 자체가 state가 되어버림 (waiting, ready, now로 바꾸고 싶자나)-->
-        <div>
-            <div>평균 시간: {{result.reduce((a,c) => a + c, 0) / result.length || 0}}ms </div> <!--다 더한 값 개수로 나눔 = 평균-->
+        <div v-if = "result.length"> <!--v-show는 태그가 있지만 눈에 안보이는 거 / v-if는 아예 없는 느낌 (보통 v-if가 더 많이 쓰임)-->
+            <div>평균 시간: {{average}}ms </div> <!--다 더한 값 개수로 나눔 = 평균 // result값을 계산해서 텍스트로 때려박음 -->
             <div>{{result}}</div>
             <button @click="onReset">리셋</button>
-        </div>
+        </div> <!--div 대신 template 쓰면 {{message}} 파트랑 평균시간, 리셋이 형제 태그로 나올 수 있음-->
     </div>
 </template>
 <script>
@@ -19,6 +19,11 @@
                 result: [], // 반응속도 시간
                 state: 'waiting',
                 message: '클릭해서 시작하세요.',
+            }
+        },
+        computed: { // 일반 data를 가공해서 쓸 때 씀 -> computed 쓰면 값이 캐싱됨
+            average() {
+                return this.result.reduce((a,c) => a + c, 0) / this.result.length || 0
             }
         },
         methods: {
